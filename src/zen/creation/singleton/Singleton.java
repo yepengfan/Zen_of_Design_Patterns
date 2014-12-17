2d28d93881e40073bd6265233a6bd72c35dc566b
Yepeng Fan <fanyepeng@yeah.net>
2014-12-17 16:53:14 +1100

    Singleton pattern package

---
+++ src/zen/creation/singleton/Singleton.java
@@ -0,0 +1,19 @@
+package zen.creation.singleton;
+
+/**
+ * Created by yepengfan on 17/12/14.
+ */
+public class Singleton {
+    private static final Singleton singleton = new Singleton();
+
+    private Singleton(){
+    }
+
+    public static Singleton getSingleton(){
+        return singleton;
+    }
+
+    public static void doSomething(){
+
+    }
+}
