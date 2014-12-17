42772a333f8e428501f9150d33897ca5fdb08b17
Yepeng Fan <fanyepeng@yeah.net>
2014-12-17 17:48:13 +1100

    Factory method pattern

---
+++ src/zen/creation/factory_method/BlackHuman.java
@@ -0,0 +1,13 @@
+package zen.creation.factory_method;
+
+/**
+ * Created by yepengfan on 17/12/14.
+ */
+public class BlackHuman implements Human {
+    public void getColor(){
+        System.out.println("The skin color of black man is black.");
+    }
+    public void talk(){
+        System.out.println("Black man can speak France.");
+    }
+}
---
+++ src/zen/creation/factory_method/WhiteHuman.java
@@ -0,0 +1,13 @@
+package zen.creation.factory_method;
+
+/**
+ * Created by yepengfan on 17/12/14.
+ */
+public class WhiteHuman implements Human {
+    public void getColor(){
+        System.out.println("The skin color of white man is white.");
+    }
+    public void talk(){
+        System.out.println("White man can speak English.");
+    }
+}
