1f6bf8e947483bc1b98c61bd8947bedc855ba2a2
Yepeng Fan <fanyepeng@yeah.net>
2014-12-18 15:36:56 +1100

    Template method pattern

---
+++ src/zen/behaviour/template_method/HummerH2Model.java
@@ -0,0 +1,19 @@
+package zen.behaviour.template_method;
+
+/**
+ * Created by yepengfan on 18/12/14.
+ */
+public class HummerH2Model extends HummerModel {
+    public void alarm(){
+        System.out.println("Hummer H2 alarming.");
+    }
+    public void engineBoom(){
+        System.out.println("Hummer H2 engine booming.");
+    }
+    public void start(){
+        System.out.println("Hummer H2 starts.");
+    }
+    public void stop(){
+        System.out.println("Hummer H2 stops.");
+    }
+}
